8c4e7c6d4770239c61631ee31cd412d038808906
gulp <gulp.air@gmail.com>
2026-03-23 06:19:34 +0300

    chore(ui): remove dead code - social/user-list.tsx [tw-10f]

--- src/components/social/index.ts
+++ src/components/social/index.ts
@@ -1,5 +1,2 @@
 export { FollowButton } from "./follow-button";
 export type { FollowButtonProps } from "./follow-button";
-
-export { UserList } from "./user-list";
-export type { UserListProps, UserListUser } from "./user-list";
